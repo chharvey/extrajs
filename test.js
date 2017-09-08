8d73a596c17d696357e20f1d523cd66e86226e4e
Chris Harvey <chrisharvey2pi@gmail.com>
2017-09-07 21:59:02 -0400

    add Element#attr() unit test

--- test.js
+++ test.js
@@ -99,6 +99,40 @@ function util_Date_format() {
   ].map((f) => Util.Date.format(new Date(), f)))
 }
 
+function element_attr() {
+  let x = new Element('span')
+  console.log(`new Element:\t`, x.html())
+  x.attr('attr1','val1')
+  console.log(`set attr1="val1":\t`, x.html())
+  x.attr('attr1')
+  console.log(`get attr1:\t`, x.attr('attr1'))
+  x.attr('attr2','')
+  console.log(`set attr2="":\t`, x.html())
+  x.attr('attr2',null)
+  console.log(`remove attr2:\t`, x.html())
+  x.attr('attr3',function () { return this.attr('attr1') })
+  console.log(`set attr3 to value of attr1:\t`, x.html())
+  x.attr({
+    attr1withobj: 'string',
+    attr2withobj: 42,
+    attr3withobj: true,
+    attr4withobj: function () { return `${this.name} is ${(!this.isVoid)?'not ':''}void` },
+    attr2: null,
+  })
+  console.log(`set/remove multiple attributes of multiple types:\t`, x.html())
+  x.attr({})
+  console.log(`pass empty object to #attr():\t`, x.html())
+  try {
+    x.attr('trying-NaN',NaN)
+  } catch (e) {
+    console.log(`failed to pass NaN to #attr(): ${e}:\t`, x.html())
+  }
+  x.attr('ternary1', (true) ? 'true' : null)
+  console.log(`set attributes with ternary:\t`, x.html())
+  x.attr('ternary2', (false) ? 'true' : null)
+  console.log(`remove attributes with ternary:\t`, x.html())
+}
+
 function element_style() {
   let x = new Element('span')
 
@@ -150,5 +184,6 @@ function element_concat() {
 // util_Object_typeOfNumber();
 // util_Object_is();
 // util_Date_format();
+// element_attr();
 // element_style();
 // element_concat();
