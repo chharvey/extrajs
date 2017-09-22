9d0b2e3f707ca7407ce13f7ad5104d289fb11588
Chris Harvey <chrisharvey2pi@gmail.com>
2017-09-21 22:58:57 -0400

    rename all source files; add test script

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const Util   = require('./_models/Util.class.js')
-Util.Element = require('./_models/Element.class.js') // CHANGED DEPRECATED
-Util.Mapp    = require('./_models/Mapp.class.js') // CHANGED OBSOLETE
-Util.Tree    = require('./_models/Tree.class.js') // CHANGED OBSOLETE
+const Util   = require('./src/Util.class.js')
+Util.Element = require('./src/Element.class.js') // CHANGED DEPRECATED
+Util.Mapp    = require('./src/Mapp.class.js') // CHANGED OBSOLETE
+Util.Tree    = require('./src/Tree.class.js') // CHANGED OBSOLETE
 
 module.exports = Util
