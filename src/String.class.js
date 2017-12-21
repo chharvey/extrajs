ab6539a06c4a9c2ff40e2bf64b7b0a4d247f1e38
Chris Harvey <chrisharvey2pi@gmail.com>
2017-12-20 21:27:20 -0500

    add xjs.String.stringify

--- src/String.class.js
+++ src/String.class.js
@@ -1,4 +1,5 @@
 const xjs = {}
+xjs.Object = require('./Object.class.js')
 
 /**
  * @summary Additional static members for the native String class.
@@ -11,6 +12,29 @@ xjs.String = class {
    */
   constructor() {}
 
+  /**
+   * @summary Convert a thing into a string.
+   * @description If the argument is an array, it is joined.
+   * If it is an object, `JSON.stringify` is called on it.
+   * This method calls `.toString()` on everything else, except `null` and `undefined` (see param notes below).
+   * Useful for JSON objects where the value could be a single string or an array of strings.
+   * @param   {*} thing anything to convert
+   * @param   {boolean=} truthy defines how to handle `null` and `undefined`:
+   *                            if `true`, they are converted to `'null'` and `'undefined'` respectively;
+   *                            else, they are converted to the empty string `''`
+   * @returns {string} a string version of the argument
+   */
+  static stringify(thing, truthy = false) {
+    const returned = {
+      'array'    : function (arg) { return arg.join('') },
+      'object'   : function (arg) { return JSON.stringify(arg) },
+      'string'   : function (arg) { return arg },
+      'null'     : function (arg) { return (truthy) ? 'null'      : '' },
+      'undefined': function (arg) { return (truthy) ? 'undefined' : '' },
+      default(arg) { return arg.toString() },
+    }
+    return (returned[xjs.Object.typeOf(thing)] || returned.default).call(null, thing)
+  }
 }
 
 module.exports = xjs.String
