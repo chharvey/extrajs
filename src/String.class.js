053b023d9bbeff936434ac6f710a6c99824a0b96
Chris Harvey <chrisharvey2pi@gmail.com>
2017-12-20 21:15:56 -0500

    new file:   src/String.class.js

---
+++ src/String.class.js
@@ -0,0 +1,16 @@
+const xjs = {}
+
+/**
+ * @summary Additional static members for the native String class.
+ * @description Does not extend the native String class.
+ * @namespace
+ */
+xjs.String = class {
+  /**
+   * @private
+   */
+  constructor() {}
+
+}
+
+module.exports = xjs.String
