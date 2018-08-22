5fa0ffcea4c0caabc65cb2a03e19237c881cb9e2
Chris Harvey <chrisharvey2pi@gmail.com>
2018-08-22 14:27:08 -0400

    fixup 5e76cbc

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@
 
 export { default as Object } from './src/class/Object.class'
 export { default as Number } from './src/class/Number.class'
+export { default as Math   } from './src/class/Math.class'
 export { default as Date   } from './src/class/Date.class'
 export { default as String } from './src/class/String.class'
 export { default as Array  } from './src/class/Array.class'
