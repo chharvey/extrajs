435ed505fe3bfbd0e53116dc0b90f6e20e14adec
Chris Harvey <chrisharvey2pi@gmail.com>
2018-08-24 00:08:06 -0400

    update gulp tasks & configs

--- gulpfile.js
+++ gulpfile.js
@@ -7,15 +7,18 @@ const typescript = require('gulp-typescript')
 const tsconfig      = require('./config/tsconfig.json')
 const typedocconfig = require('./config/typedoc.json')
 
-gulp.task('compile', async function () {
+gulp.task('test', async function () {
+})
+
+gulp.task('docs', async function () {
+  return gulp.src('./src/**/*.ts')
+    .pipe(typedoc(typedocconfig))
+})
+
+gulp.task('dist', async function () {
   return gulp.src('./src/class/*.class.ts')
     .pipe(typescript(tsconfig.compilerOptions))
     .pipe(gulp.dest('./dist/class/'))
 })
 
-gulp.task('docs', async function () {
-  return gulp.src('./src/class/*.class.ts')
-    .pipe(typedoc(typedocconfig))
-})
-
-gulp.task('build', ['compile', 'docs'])
+gulp.task('build', ['test', 'docs', 'dist'])
