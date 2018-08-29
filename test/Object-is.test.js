acee6b9af915690c718784a9ba9e98bae20df8ee
Chris Harvey <chrisharvey2pi@gmail.com>
2018-08-29 13:28:24 -0400

    modified:   test/String-stringify.test.js

--- test/Object-is.test.js
+++ test/Object-is.test.js
@@ -5,13 +5,14 @@ const xjs = require('../index.js')
 
 /**
  * @summary The master test function for this subject.
- * @see https://nodejs.org/api/assert.html#assert_assert_value_message
- * @param   {boolean} value the value to test
- * @returns {boolean} does `assert(value)` not throw?
- * @throws  {AssertionError} the error from `assert(value)`
+ * @see https://nodejs.org/api/assert.html#assert_assert_strictequal_actual_expected_message
+ * @param   {string}  actual   the actual value to test
+ * @param   {string}  expected the value that `actual` is expected to be
+ * @returns {boolean} does `assert.strictEqual(actual, expected)` not throw?
+ * @throws  {AssertionError} the error from `assert.strictEqual(actual, expected)`
  */
-async function test(value) {
-	return assert(value, `Got false, but was expecting true.`) || true
+async function test(actual, expected) {
+	return assert.strictEqual(actual, expected, `Got '${actual}', but was expecting '${expected}'.`) || true
 }
 
 let x = { a: 1, b: ['1'], c: { val: ['one'] } }
@@ -20,7 +21,7 @@ let c1 = (x_a, y_a) => assert.deepStrictEqual(x_a, y_a) || true
 let c2 = (x_a, y_a) => x_a === y_a
 
 module.exports = Promise.all([
-	test(xjs.Object.is(x, y)     === true),
-	test(xjs.Object.is(x, y, c1) === true),
-	test(xjs.Object.is(x, y, c2) === false),
+	test(`${xjs.Object.is(x, y)    }`, 'true' ),
+	test(`${xjs.Object.is(x, y, c1)}`, 'true' ),
+	test(`${xjs.Object.is(x, y, c2)}`, 'false'),
 ]).then((arr) => true)
