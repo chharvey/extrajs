6e70e2e3a8b1fdbe2d99ec6a3b5d7c003eda68c6
Chris Harvey <chrisharvey2pi@gmail.com>
2018-08-29 15:22:02 -0400

    modified:   test/Date-format.test.js

--- gulpfile.js
+++ gulpfile.js
@@ -19,6 +19,7 @@ gulp.task('test', async function () {
 			require('./test/Object-typeOf.test.js'),
 			require('./test/Object-is.test.js'),
 			require('./test/Number-typeOf.test.js'),
+			require('./test/Date-format.test.js'),
 			require('./test/String-stringify.test.js'),
 		])
 		console.log('All tests ran successfully!')
