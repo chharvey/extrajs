a0f17d43b2efe7d6fe4505c0ea134c022e29870b
Chris Harvey <chrisharvey2pi@gmail.com>
2018-08-29 15:23:37 -0400

    xjs.Array.contains returns false instead of switching args

--- src/class/Array.class.ts
+++ src/class/Array.class.ts
@@ -69,6 +69,7 @@ export default class xjs_Array {
    * xjs.Array.contains([2,'w','o',4,'o','u','r',6,'i','x'], ['o','u'])===true
    * xjs.Array.contains([2,'w','o',4,'o','u','r',6,'i','x'], [6,'o','u','r'])===false // not in the same order
    * xjs.Array.contains([2,'w','o',4,'o','u','r',6,'i','x'], [2,4,6])===false // not consecutive
+   * xjs.Array.contains([2,4,6], [2,4,6,8])===false // first array is smaller than second
    *
    * @param   larger  the larger array, to test against
    * @param   smaller the smaller array, to test
@@ -76,8 +77,8 @@ export default class xjs_Array {
    */
   static contains<T>(larger: T[], smaller: T[]): boolean {
     if (smaller.length > larger.length) {
-      console.warn('First argument cannot be smaller than the second. Switching the arguments…')
-      return xjs_Array.contains(smaller, larger)
+      console.error('First argument cannot be smaller than the second. Try switching the arguments.')
+      return false
     }
     if (xjs_Array.is(smaller, []    )) return true
     if (xjs_Array.is(smaller, larger)) return true
