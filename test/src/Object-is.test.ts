14cdccd66fbf151413e315a697674989583ce8fc
Chris Harvey <chrisharvey2pi@gmail.com>
2018-11-02 18:59:33 -0400

    handle compiletime errors

--- test/src/Object-is.test.ts
+++ test/src/Object-is.test.ts
@@ -6,7 +6,7 @@ import test from './test'
 
 let x = { a: 1, b: ['1'], c: { val: ['one'] } }
 let y = { a: 1, b: ['1'], c: { val: ['one'] } }
-let c1 = (x_a: unknown, y_a: unknown) => assert.deepStrictEqual(x_a, y_a) || true
+let c1 = (x_a: unknown, y_a: unknown) => (assert.deepStrictEqual(x_a, y_a), true)
 
 export default Promise.all([
 	test(`${xjs.Object.is(x, y)    }`, 'false'),
