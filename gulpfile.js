c7468c9820543566792fd0d260a922aaca2aefcc
Chris Harvey <chrisharvey2pi@gmail.com>
2019-02-08 21:25:40 -0500

    split task test_run into subroutines

--- gulpfile.js
+++ gulpfile.js
@@ -19,24 +19,75 @@ function test_out() {
 		.pipe(gulp.dest('./test/out/'))
 }
 
-async function test_run() {
-		await Promise.all([
-			require('./test/out/Object-typeOf.test.js')    .default,
-			require('./test/out/Object-is.test.js')        .default,
-			require('./test/out/Number-typeOf.test.js')    .default,
-			require('./test/out/Number-assertType.test.js').default,
-			require('./test/out/Math-mod.test.js')         .default,
-			require('./test/out/Math-clamp.test.js')       .default,
-			require('./test/out/Math-mean.test.js')        .default,
-			require('./test/out/Math-interpolate.test.js') .default,
-			require('./test/out/Date-format.test.js')      .default,
-			require('./test/out/String-stringify.test.js') .default,
-			require('./test/out/Array-contains.test.js')   .default,
-			require('./test/out/Array-densify.test.js')    .default,
-			require('./test/out/Array-fillHoles.test.js')  .default,
-		])
-		console.info('All tests ran successfully!')
+async function test_run_Array() {
+	await Promise.all([
+		require('./test/out/Array-contains.test.js' ).default,
+		require('./test/out/Array-densify.test.js'  ).default,
+		require('./test/out/Array-fillHoles.test.js').default,
+	])
+	console.info('All _Array_ tests ran successfully!')
 }
+
+async function test_run_Date() {
+	await Promise.all([
+		require('./test/out/Date-format.test.js').default,
+	])
+	console.info('All _Date_ tests ran successfully!')
+}
+
+async function test_run_Math() {
+	await Promise.all([
+		require('./test/out/Math-mod.test.js')        .default,
+		require('./test/out/Math-clamp.test.js')      .default,
+		require('./test/out/Math-mean.test.js')       .default,
+		require('./test/out/Math-interpolate.test.js').default,
+	])
+	console.info('All _Math_ tests ran successfully!')
+}
+
+async function test_run_Number() {
+	await Promise.all([
+		require('./test/out/Number-typeOf.test.js'    ).default,
+		require('./test/out/Number-assertType.test.js').default,
+	])
+	console.info('All _Number_ tests ran successfully!')
+}
+
+async function test_run_Object() {
+	await Promise.all([
+		require('./test/out/Object-typeOf.test.js').default,
+		require('./test/out/Object-is.test.js')    .default,
+	])
+	console.info('All _Object_ tests ran successfully!')
+}
+
+async function test_run_Set() {
+	await Promise.all([
+		require('./test/out/String-stringify.test.js').default,
+	])
+	console.info('All _Set_ tests ran successfully!')
+}
+
+async function test_run_String() {
+	await Promise.all([
+		require('./test/out/String-stringify.test.js').default,
+	])
+	console.info('All _String_ tests ran successfully!')
+}
+
+const test_run = gulp.series(
+	gulp.parallel(
+		test_run_Array,
+		test_run_Date,
+		test_run_Number,
+		test_run_Math,
+		test_run_Object,
+		test_run_Set,
+		test_run_String,
+	), function test_run0() {
+		console.info('All tests ran successfully!')
+	}
+)
 
 const test = gulp.series(test_out, test_run)
 
@@ -59,6 +110,13 @@ const build = gulp.parallel(
 module.exports = {
 	dist,
 	test_out,
+	test_run_Array,
+	test_run_Date,
+	test_run_Math,
+	test_run_Number,
+	test_run_Object,
+	test_run_Set,
+	test_run_String,
 	test_run,
 	test,
 	docs,
