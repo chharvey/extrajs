7aa9677f000bb4d89c07a0a457bed749799b62ce
Chris Harvey <chrisharvey2pi@gmail.com>
2019-02-08 21:19:39 -0500

    alphabetize exports

--- index.ts
+++ index.ts
@@ -2,12 +2,12 @@
 // It is not meant to be compiled automatically.
 // See `./index.js` for the manual output.
 
-export { default as Object } from './src/class/Object.class'
-export { default as Number } from './src/class/Number.class'
-export { default as Math   } from './src/class/Math.class'
-export { default as Date   } from './src/class/Date.class'
-export { default as String } from './src/class/String.class'
 export { default as Array  } from './src/class/Array.class'
+export { default as Date   } from './src/class/Date.class'
+export { default as Math   } from './src/class/Math.class'
+export { default as Number } from './src/class/Number.class'
+export { default as Object } from './src/class/Object.class'
 export { default as Set    } from './src/class/Set.class'
+export { default as String } from './src/class/String.class'
 
 export { default as NaNError } from './src/class/NaNError.class'
