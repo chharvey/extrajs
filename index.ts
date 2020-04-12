6154eac9ad4de5ee126a90a58b191669a5a38ec2
Chris Harvey <chrisharvey2pi@gmail.com>
2020-04-12 12:41:14 -0400

    update exports

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@
 // See `./index.js` for the manual output.
 
 export { default as Array  } from './src/class/Array.class'
+export { default as BigInt } from './src/class/BigInt.class'
 export { default as Date   } from './src/class/Date.class'
 export { default as Map    } from './src/class/Map.class'
 export { default as Math   } from './src/class/Math.class'
@@ -12,5 +13,6 @@ export { default as Promise } from './src/class/Promise.class'
 export { default as Set    } from './src/class/Set.class'
 export { default as String } from './src/class/String.class'
 
+export {NumericType} from './src/class/Number.class'
 export { default as IndexOutOfBoundsError } from './src/class/IndexOutOfBoundsError.class'
 export { default as NaNError } from './src/class/NaNError.class'
