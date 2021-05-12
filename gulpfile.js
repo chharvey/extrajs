d6e02ca4c6d2ec42e1bb7dd659d5b0dd77b7c6ac
Chris Harvey <chrisharvey2pi@gmail.com>
2021-05-11 23:16:04 -0400

    convert "test" npm script

--- gulpfile.js
+++ gulpfile.js
@@ -5,25 +5,16 @@ const typedoc    = require('gulp-typedoc')
 
 const tsconfig      = require('./tsconfig.json')
 
-function test() {
-	return gulp.src('./test/*.ts')
-		.pipe(mocha({
-			require: 'ts-node/register',
-		}))
-}
-
 function docs() {
   return gulp.src('./src/**/*.ts')
     .pipe(typedoc(tsconfig.typedocOptions))
 }
 
 const build = gulp.parallel(
-	test,
 	docs,
 )
 
 module.exports = {
-	test,
 	docs,
 	build,
 }
