7c2aafcefa7982b89d7e59b69eb364765d6040c7
Chris Harvey <chrisharvey2pi@gmail.com>
2021-05-12 02:08:20 -0400

    uninstall gulp-typedoc

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,4 @@
 const gulp  = require('gulp')
-const typedoc    = require('gulp-typedoc')
-// require('typedoc')    // DO NOT REMOVE … peerDependency of `gulp-typedoc`
-
-const tsconfig      = require('./tsconfig.json')
 
 const build = gulp.parallel(
 )
